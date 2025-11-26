165395b549319d9fd93504214775510ca37220a7
Harif Baena <harif_1998@hotmail.com>
2025-11-26 03:40:09 -0800

    Main design changes and forms files creation

---
+++ OnLive/Views/CreateTicketForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnLive.Views
+{
+    public partial class CreateTicket : Form
+    {
+        public CreateTicket()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OnLive/Views/MainForm.cs
@@ -0,0 +1,57 @@
+using OnLive.Views;
+
+namespace OnLive
+{
+    public partial class MainForm : Form
+    {
+        SaleZoneForm salesZone;
+        UserHistoryForm userHistory;
+        CreateTicketForm createTicket;
+        ReportsForm reports;
+        ConfigForm config;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelRoundCorner1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
